034d91b1315d263ed6aaaf1b8ccf7c5439675ae8
Remco Mensinck <remcomensinck@MacBook-Pro-van-Remco.local>
2023-04-07 12:58:38 +0200

    tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,126 @@
 import Head from 'next/head'
-import styles from '@/styles/Home.module.css'
 import { useState } from 'react';
 
+const info_texts = {
+  "level": {
+    1: "I never skied before.",
+    2: "I never skied before.",
+    3: "I've skied a couple of weeks.",
+    4: "I've skied a couple of weeks.",
+    5: "I ski 1-10 days a year.",
+    6: "I ski 1-10 days a year.",
+    7: "I ski multiple weeks every year.",
+    8: "I ski multiple weeks every year.",
+    9: "I live in the mountains!",
+    10: "I live in the mountains!",
+  },
+  "piste": {
+    1: "I never ski on piste.",
+    2: "I never ski on piste.",
+    3: "I don't care about piste.",
+    4: "I don't care about piste.",
+    5: "I ski piste occasionally.",
+    6: "I ski piste occasionally.",
+    7: "I spend most days on piste.",
+    8: "I spend most days on piste.",
+    9: "Skiing slopes is my life!",
+    10: "Skiing slopes is my life!",
+  },
+  "powder": {
+    1: "I never ski powder.",
+    2: "I never ski powder.",
+    3: "I like fresh snow on the slopes.",
+    4: "I like fresh snow on the slopes.",
+    5: "I like to ski between slopes",
+    6: "I like to ski between slopes",
+    7: "I like long powder runs",
+    8: "I like long powder runs",
+    9: "I travel for deep powder!",
+    10: "I travel for deep powder!",
+  },
+  "freeride": {
+    1: "I dont care about freeride.",
+    2: "I dont care about freeride.",
+    3: "I like to ski trails in the forrest",
+    4: "I like to ski trails in the forrest",
+    5: "I occasionally go off-piste",
+    6: "I occasionally go off-piste",
+    7: "I'm often looking cool lines",
+    8: "I'm often looking cool lines",
+    9: "I ride freeride competitions!",
+    10: "I ride freeride competitions!",
+  },
+  "touring": {
+    1: "I never go skitouring.",
+    2: "I never go skitouring.",
+    3: "I might try skitouring once.",
+    4: "I might try skitouring once.",
+    5: "I go touring once or twice",
+    6: "I go touring once or twice",
+    7: "I like skitouring regularly",
+    8: "I like skitouring regularly",
+    9: "Skitouring is all i do!",
+    10: "Skitouring is all i do!",
+  },
+  "park": {
+    1: "I never ski in the funpark.",
+    2: "I never ski in the funpark.",
+    3: "I might try a funpark lap.",
+    4: "I might try a funpark lap.",
+    5: "I occasionally ride park.",
+    6: "I occasionally ride park.",
+    7: "I regularly ride park",
+    8: "I regularly ride park",
+    9: "I spend most time in the park!",
+    10: "I spend most time in the park!",
+  },
+  "playfull": {
+    1: "I like high-performance ski's!",
+    2: "I like high-performance ski's!",
+    3: "I'm a strong, fast skier.",
+    4: "I'm a strong, fast skier.",
+    5: "I like a stable, forgiving ski.",
+    6: "I like a stable, forgiving ski.",
+    7: "I like a playfull ski.",
+    8: "I like a playfull ski.",
+    9: "I just butter and jump around!",
+    10: "I just butter and jump around!",
+  }
+}
 
 export default function Home() {
-  const start_value: number = 5
-  const [level, setLevel] = useState(start_value)
-  const [playfull, setPlayfull] = useState(start_value)
-  const [piste, setPiste] = useState(start_value)
-  const [park, setPark] = useState(start_value)
-  const [powder, setPowder] = useState(start_value)
-  const [freeride, setFreeride] = useState(start_value)
-  const [touring, setTouring] = useState(start_value)
+  const playfull_default = 5
+  const level_default = 1
+  const piste_default = 7
+  const powder_default = 1
+  const freeride_default = 1
+  const park_default = 1
+  const touring_default = 1
+  const [level, setLevel] = useState(level_default)
+  const [playfull, setPlayfull] = useState(playfull_default)
+  const [piste, setPiste] = useState(piste_default)
+  const [park, setPark] = useState(park_default)
+  const [powder, setPowder] = useState(powder_default)
+  const [freeride, setFreeride] = useState(freeride_default)
+  const [touring, setTouring] = useState(touring_default)
   const [scores, setScores] = useState([])
   const [loading, setLoading] = useState(false)
+  const [pisteInfo, setPisteInfo] = useState(info_texts.piste[piste_default])
+  const [powderInfo, setPowderInfo] = useState(info_texts.powder[powder_default])
+  const [freerideInfo, setFreerideInfo] = useState(info_texts.freeride[freeride_default])
+  const [parkInfo, setParkInfo] = useState(info_texts.park[park_default])
+  const [touringInfo, setTouringInfo] = useState(info_texts.touring[touring_default])
+  const [levelInfo, setLevelInfo] = useState(info_texts.level[level_default])
+  const [playfullInfo, setPlayfullInfo] = useState(info_texts.playfull[playfull_default])
+
+  interface InputField {
+    name: string;
+    id: string;
+    setFunction: (value: any) => void;
+    default_val: number;
+    info: string;
+    setInfo: (value: any) => void;
+  }
 
   const getScores = async () => {
     setLoading(true)
@@ -31,14 +138,14 @@ export default function Home() {
     setLoading(false)
   }
 
-  const inputFields = [
-    { "name": "Piste", "id": "piste", "setFunction": setPiste },
-    { "name": "Park", "id": "park", "setFunction": setPark },
-    { "name": "Powder", "id": "powder", "setFunction": setPowder },
-    { "name": "Freeride", "id": "freeride", "setFunction": setFreeride },
-    { "name": "Touring", "id": "touring", "setFunction": setTouring },
-    { "name": "User level", "id": "level", "setFunction": setLevel },
-    { "name": "Playfullness", "id": "playfull", "setFunction": setPlayfull },
+  const inputFields: InputField[] = [
+    { "name": "Piste", "id": "piste", "setFunction": setPiste, "default_val": piste_default, "info": pisteInfo, "setInfo": setPisteInfo },
+    { "name": "Park", "id": "park", "setFunction": setPark, "default_val": park_default, "info": parkInfo, "setInfo": setParkInfo },
+    { "name": "Powder", "id": "powder", "setFunction": setPowder, "default_val": powder_default, "info": powderInfo, "setInfo": setPowderInfo },
+    { "name": "Freeride", "id": "freeride", "setFunction": setFreeride, "default_val": freeride_default, "info": freerideInfo, "setInfo": setFreerideInfo },
+    { "name": "Touring", "id": "touring", "setFunction": setTouring, "default_val": touring_default, "info": touringInfo, "setInfo": setTouringInfo },
+    { "name": "User level", "id": "level", "setFunction": setLevel, "default_val": level_default, "info": levelInfo, "setInfo": setLevelInfo },
+    { "name": "Playfullness", "id": "playfull", "setFunction": setPlayfull, "default_val": playfull_default, "info": playfullInfo, "setInfo": setPlayfullInfo },
   ]
 
   return (
@@ -46,43 +153,51 @@ export default function Home() {
       <Head>
         <title>Ski Selector</title>
       </Head>
-      <main className={styles.main}>
-        <h1 className={styles.title}>Ski Selector</h1>
-        <p className={styles.description}>
-          Find the best skis for you!
-        </p>
-        <div className={styles.grid}>
-          {inputFields.map((input, index) => (
-            <div key={index} className={styles.inputContainer}>
-              <label htmlFor={input.id} className={styles.label}>
-                {input.name}
-              </label>
-              <input
-                type="range"
-                id={input.id}
-                name={input.id}
-                min="0"
-                max="10"
-                className={styles.slider}
-                onChange={(val) => input.setFunction(Number(val.target.value))}
-              />
-            </div>
-          ))}
-        </div>
-        <button className={styles.button} onClick={() => getScores()}>
-          {loading ? 'Loading...' : 'Submit'}
-        </button>
-        {scores.length > 0 && (
-          <div className={styles.scores}>
-            <h2>Your Top Ski Choices</h2>
+      <main className="p-6">
+        <div className="grid place-items-center rounded-xl py-4 bg-slate-100">
+          <h1 className="text-4xl font-bold mb-4">Ski Selector</h1>
+          <p className="text-gray-600 mb-6">
+            Find the best skis for you!
+          </p>
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-2 max-w-lg ">
+            {inputFields.map((input, index) => (
+              <div key={index}>
+                <label htmlFor={input.id} className="text-gray-600 block mb-2">
+                  {`"${input.info}"`}
+                </label>
+                <input
+                  type="range"
+                  id={input.id}
+                  name={input.id}
+                  defaultValue={input.default_val.toString()}
+                  min="1"
+                  max="10"
+                  onChange={(val) => {
+                    input.setFunction(Number(val.target.value))
+                    input.setInfo(info_texts[input.id][val.target.value])
+                  }
+                  }
+                  className="w-60"
+                />
+              </div>
+            ))}
+          </div>
+          <button onClick={() => getScores()} className="bg-blue-500 text-white py-2 px-4 rounded-full mt-6 hover:bg-blue-700 transition duration-300">
+            {loading ? 'Loading...' : 'Submit'}
+          </button>
+          <div className={`mt-6 bg-slate-200 rounded-xl  transition-all ease-in duration-700 ${scores.length > 0 ? 'opacity-100 px-4 py-4' : 'opacity-0'}`}>
+            <h2 className="text-2xl font-bold mb-2">Your Top Ski Choices</h2>
             {scores.slice(0, 5).map((ski, index) => (
-              <p key={index}>
+              <p key={index} className="text-gray-600">
                 {ski["score"]} {ski["name"]}
               </p>
             ))}
           </div>
-        )}
+        </div>
       </main>
     </>
   )
 }
+
+
+
