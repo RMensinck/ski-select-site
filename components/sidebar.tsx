c2c9ea0914384597a8f959a7629a29be03e08f4c
Remco Mensinck <remcomensinck@MacBook-Pro-van-Remco.local>
2023-04-21 16:51:37 +0200

    a to link

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,6 +1,7 @@
 import { FaPaperPlane, FaRoute, FaHome, FaBars, FaMicroscope } from 'react-icons/fa'
 import texts from '../components/textsSidebar'
 import { useRouter } from 'next/router'
+import Link from 'next/link'
 
 
 
@@ -15,9 +16,9 @@ const SideBar = () => {
             <div className="fixed top-0 left-0 h-screen  w-16 m-0 flex flex-col 
                             bg-slate-100 text-white shadow-lg group:hover:z-20 bg-opacity-50
                             scale-0 group-hover:scale-100 transition-all duration-200" >
-                <a href="/"><SideBarIcon icon={<FaHome size="28"/>} text={texts.home[locale]}/></a>
-                <a href="/mission"><SideBarIcon icon={<FaRoute size="28"/>} text={texts.mission[locale]}/></a>
-                <a href="/contact"><SideBarIcon icon={<FaPaperPlane size="28"/>} text={texts.contact[locale]}/></a>
+                <Link href="/"><SideBarIcon icon={<FaHome size="28"/>} text={texts.home[locale]}/></Link>
+                <Link href="/mission"><SideBarIcon icon={<FaRoute size="28"/>} text={texts.mission[locale]}/></Link>
+                <Link href="/contact"><SideBarIcon icon={<FaPaperPlane size="28"/>} text={texts.contact[locale]}/></Link>
                 <SideBarIcon icon={<FaMicroscope size="28"/>} text={texts.reviews[locale]}/>
             </div>
         </div>
