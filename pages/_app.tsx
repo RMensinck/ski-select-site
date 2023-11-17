95bee77b4e8ed36d7fc9254d58bd8de32640470b
Remco Mensinck <remcomensinck@MBP-van-Remco.home>
2023-11-17 19:51:22 +0100

    cookie banner

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,14 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Script from 'next/script'
 import { initFirebase } from '../firebaseConfig'
+import CookieConsent from 'react-cookie-consent'
+import CookieText from '../texts/textsCookieBanner'
+import { useRouter } from 'next/router'
 
 export default function App({ Component, pageProps }: AppProps) {
   initFirebase()
+  const router = useRouter()
+  const { locale } = router 
   return (
   <>
     <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-SFCQQ8FEPR"/>
@@ -23,6 +28,16 @@ export default function App({ Component, pageProps }: AppProps) {
       }}
     />
     <Component {...pageProps} />
+    <CookieConsent
+      location='bottom'
+      buttonText={CookieText.accept[locale]}
+      cookieName="AcceptCookies"
+      style={{ background: "#274c77" }}
+      buttonStyle={{ background: "#e7ecef", color: "#4e503b", fontSize: "13px" }}
+      expires={150}
+    >
+      <p className=' text-xs'>{CookieText.main[locale]}</p>
+    </CookieConsent>
   </>
   )
 }
---
+++ texts/textsCookieBanner.tsx
@@ -0,0 +1,12 @@
+const textsProfile = {
+    main: {
+        "en": "We use cookies on our website. These cookies are strictly necessary for the site to function. In addition, we use Google Analytics to understand how our site is used, which helps us improve your browsing experience.",
+        "nl": "Wij gebruiken cookies op deze website. Deze cookies zijn strikt noodzakelijk voor de werking van de site. Daarnaast gebruiken we Google Analytics om te begrijpen hoe onze site wordt gebruikt, wat ons helpt uw browse-ervaring te verbeteren.",
+    },
+    accept: {
+        "en": "Accept",
+        "nl": "Accepteren",
+    },
+}
+  
+  export default textsProfile
