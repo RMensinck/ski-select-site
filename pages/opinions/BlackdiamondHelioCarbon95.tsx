af02308dbc9f7d5db84d84c8531197fe67ac40e2
Remco Mensinck <remcomensinck@hotmail.com>
2024-02-06 14:50:27 +0100

    bd helio type fix

--- pages/opinions/BlackdiamondHelioCarbon95.tsx
+++ pages/opinions/BlackdiamondHelioCarbon95.tsx
@@ -1,5 +1,5 @@
 import opinionsTemplate from '../../components/opinionsTemplate'
 
             export default function Opinions() {
-                return opinionsTemplate("Black diamond Helio Carbon 95")
+                return opinionsTemplate("Black diamond Helio carbon 95")
             }
