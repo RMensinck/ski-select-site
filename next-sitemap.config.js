391e07596d51f354016daec902109f67565adc5a
Remco Mensinck <remcomensinck@hotmail.com>
2024-02-07 17:04:27 +0100

    sitemap changes

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,5 +2,26 @@
 module.exports = {
     siteUrl: 'https://www.pick-a-ski.com',
     generateRobotsTxt: true, // (optional)
-    // ...other options
+    i18n: {
+      locales: ['en', 'nl', 'de', 'fr', 'pl', 'ja', 'es'], // List your locales here
+      defaultLocale: 'en',
+    },
+    transform: async (config, path) => {
+      // Correct the path for locale-specific URLs
+      const correctedPath = path === '/' ? '' : path; // Adjusts root path correctly
+    
+      return {
+        loc: `${config.siteUrl}${correctedPath}`, // Ensure the correct base URL
+        changefreq: 'daily',
+        priority: 0.7,
+        lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+        
+
+        alternateRefs: ["/nl", "/de", "/es", "/pl", "/ja", "/en", "/fr"].includes(path) ? [] : config.i18n.locales.map((locale) => {
+          // dont add alternates for example.com/nl or example.com/en
+          const href = `${config.siteUrl}/${locale}${correctedPath}`;
+          return { href, hreflang: locale };
+        }),
+      }
+    }
   }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,28 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    // Access the locale from the context
+    const locale = ctx.locale || 'en'; // Fallback to 'en' if locale is not available
+
+    return { ...initialProps, locale };
+  }
+
+  render() {
+    // Use the locale for setting the `lang` attribute
+    const { locale } = this.props as any; // Cast to any to access custom props
+
+    return (
+      <Html lang={locale}>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
