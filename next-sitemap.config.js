f180b42478aa5e9c826651c2769c6d4b55e1664f
Remco Mensinck <remcomensinck@hotmail.com>
2024-02-08 00:35:58 +0100

    okish sitemap design

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,27 +1,39 @@
 /** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: 'https://www.pick-a-ski.com',
-    generateRobotsTxt: true, // (optional)
-    i18n: {
-      locales: ['en', 'nl', 'de', 'fr', 'pl', 'ja', 'es'], // List your locales here
-      defaultLocale: 'en',
-    },
-    transform: async (config, path) => {
-      // Correct the path for locale-specific URLs
-      const correctedPath = path === '/' ? '' : path; // Adjusts root path correctly
-    
-      return {
-        loc: `${config.siteUrl}${correctedPath}`, // Ensure the correct base URL
-        changefreq: 'daily',
-        priority: 0.7,
-        lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
-        
 
-        alternateRefs: ["/nl", "/de", "/es", "/pl", "/ja", "/en", "/fr"].includes(path) ? [] : config.i18n.locales.map((locale) => {
-          // dont add alternates for example.com/nl or example.com/en
-          const href = `${config.siteUrl}/${locale}${correctedPath}`;
-          return { href, hreflang: locale };
-        }),
-      }
+const siteUrl = 'https://www.pick-a-ski.com';
+module.exports = {
+  siteUrl: 'https://www.pick-a-ski.com',
+  generateRobotsTxt: true, // Generate robots.txt
+  sitemapSize: 7000, // Split sitemap into multiple files if you have more URLs
+  outDir: './public', // Output directory for the generated sitemap files
+  // Specify how to generate alternate links (locales) for each URL
+  transform: async (config, path) => {
+    // Define your locales
+    
+    const locales = ['en', 'fr', 'de', 'es', 'nl', 'pl', 'ja'];
+    let alternateRefs = locales.map(locale => ({
+      href: `${config.siteUrl}/${locale}${path}`,
+      hreflang: locale,
+    }));
+    if (["/nl", "/pl", "/ja", "/es", "/de", "/fr"].includes(path)) {
+      alternateRefs = []
     }
-  }
+
+    return {
+      loc: path, // Original path
+      alternateRefs, // Alternate references for different locales
+    };
+  },
+  // Additional options can be defined here, such as `exclude` to omit certain paths
+  exclude: ['/404', '/404.html'], // Example of excluding specific paths
+  robotsTxtOptions: {
+    policies: [
+      { userAgent: '*', allow: '/' },
+    ],
+    additionalSitemaps: [
+      // If you have additional sitemaps (e.g., dynamic sitemaps), list them here
+      `${siteUrl}/sitemap.xml`, // Main sitemap
+      // Example: `${siteUrl}/server-sitemap.xml`, // If you have a server-generated sitemap
+    ],
+  },
+};
