616938f8042435013c89a4923f8ea81dfe342fc7
Remco Mensinck <remcomensinck@hotmail.com>
2024-02-09 14:07:55 +0100

    google tag manager

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,6 +17,12 @@ class MyDocument extends Document {
       <Html lang={locale}>
         <Head />
         <body>
+          <noscript dangerouslySetInnerHTML={{
+            __html: `
+              <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N39BC53F"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>
+            `,
+          }} />
           <Main />
           <NextScript />
         </body>
