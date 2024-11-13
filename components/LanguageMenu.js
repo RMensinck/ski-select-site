5c984d17450f7f0711354702c7f1cb325c50a1dd
Remco <remco@MacBook-Air-van-Remco.local>
2024-11-13 23:47:59 +0100

    bentchetler image spelling

--- components/LanguageMenu.js
+++ components/LanguageMenu.js
@@ -34,6 +34,7 @@ export default function Example() {
     }
   }
   return (
+    //test
     <Menu as="div" className="relative inline-block text-left">
       <div>
         <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
