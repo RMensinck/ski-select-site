d40ca8c867c7e8ea1e2baccf838094a64448f8bd
Remco <remco@MacBook-Air-van-Remco.local>
2024-11-18 01:19:30 +0100

    corrected skis json

--- pages/opinions/AtomicBentchetler120.tsx
+++ pages/opinions/AtomicBentchetler120.tsx
@@ -2,4 +2,4 @@ import opinionsTemplate from '../../components/opinionsTemplate'
 
             export default function Opinions() {
                 return opinionsTemplate("Volkl Revolt 86")
-            }}}   } }  }}  }
+            } }}  } }  }}  }
