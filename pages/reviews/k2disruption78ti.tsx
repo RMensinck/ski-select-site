bafe23388805165d1372d30e942f5737fb9381f6
Remco <remco@MacBook-Air-van-Remco.local>
2024-11-24 22:41:57 +0100

    mindbender reviews

---
+++ pages/reviews/k2disruption78ti.tsx
@@ -0,0 +1,24 @@
+import ReviewTemplate from '../../components/reviewTemplate'
+import { GetServerSideProps } from 'next';
+import { checkFolderExists } from '../../utils/checkFolderExists';
+import reviews from '../../public/reviews/reviews.json'
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+
+    const photoFolderExists = checkFolderExists(reviews["K2 Disruption 78Ti"].brand, reviews["K2 Disruption 78Ti"].model);
+
+    return {
+        props: {
+        photoFolderExists,
+        },
+    };
+    };
+
+interface ReviewProps {
+  skiName: string;
+  photoFolderExists: boolean;
+}
+
+export default function Review({ photoFolderExists }: ReviewProps) {
+    return ReviewTemplate("K2 Disruption 78Ti", photoFolderExists)
+}
